32c0217dc83c9c92f494e8187065337091ac09c6
sherby <sherby@bitbucket.org>
2016-05-12 18:36:00 +0100

    cont.

--- teste.sh
+++ teste.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-S="sudo"
 I="/sbin/iptables"
 
-$S $I -F
+$I -F
 # already established
-$S $I -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-$S $I -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$I -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$I -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+echo "already established"
 # clients 
-$S $I -A INPUT -p tcp --dport 23457 -j ACCEPT
+$I -A INPUT -p tcp --dport 23457 -j ACCEPT
+echo "clients"
 # ssh gcc
-$S $I -A INPUT -s 10.101.151.5 -p tcp --dport ssh -j ACCEPT
+$I -A INPUT -s 10.101.151.5 -p tcp --dport ssh -j ACCEPT
+echo "ssh gcc"
 # icmp gcc
-$S $I -A -OUTPUT -d 10.101.151.5 -p icmp -j ACCEPT
+$I -A -OUTPUT -d 10.101.151.5 -p icmp -j ACCEPT
+echo "icmp gcc"
 # acessos default
-$S $I -A INPUT -d "10.101.253.11, 10.101.253.12, 10.101.253.13, 10.121.53.14, 10.121.53.15, 10.101.53.16, 10.101.249.63, 10.101.85.6, 10.101.85.138, 10.101.85.18, 10.101.148.1, 10.101.85.134" -j ACCEPT
-$S $I -A OUTPUT -d "10.101.253.11, 10.101.253.12, 10.101.253.13, 10.121.53.14, 10.121.53.15, 10.101.53.16, 10.101.249.63, 10.101.85.6, 10.101.85.138, 10.101.85.18, 10.101.148.1, 10.101.85.134" -j ACCEPT
+$I -A INPUT -d "10.101.253.11, 10.101.253.12, 10.101.253.13, 10.121.53.14, 10.121.53.15, 10.101.53.16, 10.101.249.63, 10.101.85.6, 10.101.85.138, 10.101.85.18, 10.101.148.1, 10.101.85.134" -j ACCEPT
+$I -A OUTPUT -d "10.101.253.11, 10.101.253.12, 10.101.253.13, 10.121.53.14, 10.121.53.15, 10.101.53.16, 10.101.249.63, 10.101.85.6, 10.101.85.138, 10.101.85.18, 10.101.148.1, 10.101.85.134" -j ACCEPT
+echo "acessos default"
 # unfilter loopback
-$S $I -A INPUT -i lo -j ACCEPT
-$S $I -A OUTPUT -o lo -j ACCEPT
+$I -A INPUT -i lo -j ACCEPT
+$I -A OUTPUT -o lo -j ACCEPT
+echo "unfilter loopback"
 # politica fechada
-$S $I -A INPUT  -j DROP
-$S $I -A OUTPUT -j DROP
+$I -A INPUT  -j DROP
+$I -A OUTPUT -j DROP
+echo "politica fechada"
